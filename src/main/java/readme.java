f2d62793d4e34e0e2215ed5ddc714f12e1990282
yamini68 <littlemajesty684@gmail.com>
2023-09-07 19:52:29 +0530

    archtect from yamini team

--- src/main/java/readme.java
+++ src/main/java/readme.java
@@ -1,6 +1,19 @@
+import org.testng.annotations.Test;
+
 public class readme {
     public static void main(String [] args){
         System.out.println("hello this is my Seleium project so i have added lot of stuff");
         System.out.println("I am murali i have added some report in it to understand the testcase better");
     }
+    @Test
+    public void hello(){
+        System.out.println("design yamini");
+    }
+
+    @Test
+    public void hello1(){
+        System.out.println("design yamini photo");
+    }
+
+
 }
